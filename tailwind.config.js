e33849d7418ad4f75b9e4d9eb12c08f40cd609fc
soga-johnny <soga.johnny@gmail.com>
2025-03-31 20:32:10 +0900

    fix:imports15

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'opacity-0',
+    'translate-y-4',
+    'opacity-100',
+    'fade-out',
+    'line-through',
+    'font-normal',
+    'hover:opacity-70',
+    'hover:translate-y-[-2px]',
+    'text-[#00E7A2]',
+    'text-[#FFB800]',
+    'text-[#EC5D49]',
+  ],
   theme: {
     extend: {
       fontFamily: {
